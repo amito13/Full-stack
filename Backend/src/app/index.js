2ee00a0aa0efa1d0794b03aad67231056ba8e7d0
AmitDewangan <dewanganamit40@gmail.com>
2026-05-12 00:47:05 +0530

    cors update

--- Backend/src/app/index.js
+++ Backend/src/app/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 export function createApp() {
     const app = express();
     app.use(cors({
-         origin: "http://localhost:5173"
+         origin: "https://regi-delta.vercel.app/",
     }));
     app.get('/health', (req, res) => {
         res.json({ status: 'OK' });
